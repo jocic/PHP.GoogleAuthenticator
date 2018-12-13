62f3d590e67bfc3ee60d83fd312cf5040e689020
Đorđe Jocić <office@djordjejocic.com>
2018-12-13 14:49:30 +0100

    Added autloader.

---
+++ src/Autoload.php
@@ -0,0 +1,68 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2018                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    // Step 1 - Define Autoload Function.
+    
+    /**
+     * Automatically loads a class for the library.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2018 All Rights Reserved
+     * @version   1.0.0
+     * 
+     * @param string $class_name
+     *   Name of the class, ex. <i>Security\Mfa\GoogleAuthenticator</i>.
+     * @return void
+     */
+    
+    function google_authenticator_loader($class_name)
+    {
+        // Core Variables
+        
+        $class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name) . ".class.php";
+        $class_file = join(DIRECTORY_SEPARATOR, [
+            __DIR__,
+            $class_name
+        ]);
+        
+        // Logic
+        
+        if (file_exists($class_file))
+        {
+            include $class_file;
+        }
+    }
+    
+    // Step 2 - Register Autoload Function
+    
+    spl_autoload_register("google_authenticator_loader");
+    
+?>
