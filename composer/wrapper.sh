4610f0096c3e46c5459dc3b8e7c722e01ea963bc
Đorđe Jocić <office@djordjejocic.com>
2018-12-13 14:23:11 +0100

    Added composer to the project.

---
+++ composer/wrapper.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+###################################################################
+# Script Author: Djordje Jocic                                    #
+# Script Year: 2018                                               #
+# Script License: MIT License (MIT)                               #
+# =============================================================== #
+# Personal Website: http://www.djordjejocic.com/                  #
+# =============================================================== #
+# Permission is hereby granted, free of charge, to any person     #
+# obtaining a copy of this software and associated documentation  #
+# files (the "Software"), to deal in the Software without         #
+# restriction, including without limitation the rights to use,    #
+# copy, modify, merge, publish, distribute, sublicense, and/or    #
+# sell copies of the Software, and to permit persons to whom the  #
+# Software is furnished to do so, subject to the following        #
+# conditions.                                                     #
+# --------------------------------------------------------------- #
+# The above copyright notice and this permission notice shall be  #
+# included in all copies or substantial portions of the Software. #
+# --------------------------------------------------------------- #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     #
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    #
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   #
+# OTHER DEALINGS IN THE SOFTWARE.                                 #
+###################################################################
+
+##################
+# CORE VARIABLES #
+##################
+
+source_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd);
+
+######################
+# COMPOSER VARIABLES #
+###################### 
+
+composer_version="1.7.2";
+
+#########
+# LOGIC #
+#########
+
+if [[ $(whereis php) == "php:" ]]; then
+    echo "Error: You don't have PHP installed on your machine." && exit;
+fi
+
+cd $source_dir && php "$composer_version/composer.phar" "$@";
