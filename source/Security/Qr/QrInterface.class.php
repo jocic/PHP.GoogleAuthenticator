455ce1300b8873f8b8263270c3b96a470e4d3994
Đorđe Jocić <office@djordjejocic.com>
2018-12-22 16:00:35 +0100

    Added interface for the qr classes.

---
+++ source/Security/Qr/QrInterface.class.php
@@ -0,0 +1,82 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2018                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    namespace Security\Qr;
+    
+    /**
+     * <i>QrInterface</i> is an interface used to enforce implementation of core
+     * methods used for QR code creation.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2018 All Rights Reserved
+     * @version   1.0.0
+     */
+    
+    interface QrInterface
+    {
+        /***************\
+        |* GET METHODS *|
+        \***************/
+        
+        public function getValue();
+        
+        public function getStorageDirectory();
+        
+        public function getLocation();
+        
+        /***************\
+        |* SET METHODS *|
+        \***************/
+        
+        public function setValue();
+        
+        public function setStorageDirectory();
+        
+        /****************\
+        |* CORE METHODS *|
+        \****************/
+        
+        public function generate();
+        
+        /*****************\
+        |* CHECK METHODS *|
+        \*****************/
+        
+        // CHECK METHODS GO HERE
+        
+        /*****************\
+        |* OTHER METHODS *|
+        \*****************/
+        
+        // OTHER METHODS GO HERE
+    }
+    
+?>
