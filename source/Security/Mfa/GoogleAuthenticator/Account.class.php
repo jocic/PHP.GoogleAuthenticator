c20cf55ec5df00491bdbdf9929f086d629a8e33e
Đorđe Jocić <office@djordjejocic.com>
2018-12-23 03:16:30 +0100

    Added class skeleton for account class.

---
+++ source/Security/Mfa/GoogleAuthenticator/Account.class.php
@@ -0,0 +1,94 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2018                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    namespace Security\Mfa\GoogleAuthenticator;
+    
+    /**
+     * <i>Account</i> class is used for specifying various account-related
+     * information required for <i>2FA</i> implementation.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2018 All Rights Reserved
+     * @version   1.0.0
+     */
+    
+    class Account
+    {
+        /******************\
+        |* CORE CONSTANTS *|
+        \******************/
+        
+        // CORE CONSTANTS GO HERE
+        
+        /******************\
+        |* CORE VARIABLES *|
+        \******************/
+        
+        // CORE VARIABLES GO HERE
+        
+        /*******************\
+        |* MAGIC FUNCTIONS *|
+        \*******************/
+        
+        // MAGIC FUNCTIONS GO HERE
+        
+        /***************\
+        |* GET METHODS *|
+        \***************/
+        
+        // GET METHODS GO HERE
+        
+        /***************\
+        |* SET METHODS *|
+        \***************/
+        
+        // SET METHODS GO HERE
+        
+        /****************\
+        |* CORE METHODS *|
+        \****************/
+        
+        // CORE METHODS GO HERE
+        
+        /*****************\
+        |* CHECK METHODS *|
+        \*****************/
+        
+        // CHECK METHODS GO HERE
+        
+        /*****************\
+        |* OTHER METHODS *|
+        \*****************/
+        
+        // OTHER METHODS GO HERE
+    }
+    
+?>
