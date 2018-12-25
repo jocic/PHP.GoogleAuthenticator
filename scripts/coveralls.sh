f57ab9ecb0447d51d87707543710d6b12912a330
Đorđe Jocić <office@djordjejocic.com>
2018-12-25 02:10:37 +0100

    Added PHP Coveralls to the project.

---
+++ scripts/coveralls.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+###################################################################
+# Script Author: Djordje Jocic                                    #
+# Script Year: 2018                                               #
+# Script License: MIT License (MIT)                               #
+# =============================================================== #
+# Personal Website: http://www.djordjejocic.com/                  #
+# =============================================================== #
+# Permission is hereby granted, free of charge, to any person     #
+# obtaining a copy of this software and associated documentation  #
+# files (the "Software"), to deal in the Software without         #
+# restriction, including without limitation the rights to use,    #
+# copy, modify, merge, publish, distribute, sublicense, and/or    #
+# sell copies of the Software, and to permit persons to whom the  #
+# Software is furnished to do so, subject to the following        #
+# conditions.                                                     #
+# --------------------------------------------------------------- #
+# The above copyright notice and this permission notice shall be  #
+# included in all copies or substantial portions of the Software. #
+# --------------------------------------------------------------- #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     #
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    #
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   #
+# OTHER DEALINGS IN THE SOFTWARE.                                 #
+###################################################################
+
+##################
+# Core Variables #
+##################
+
+source_dir="$(cd -- "$(dirname -- "$0")" && pwd -P)";
+
+#################
+# PHP Variables #
+#################
+
+php_command="$(command -v php)";
+
+#######################
+# Coveralls Variables #
+#######################
+
+coveralls_location="./dependencies/bin/php-coveralls";
+
+#########
+# LOGIC #
+#########
+
+cd "$source_dir/../";
+
+if [ -z "$php_command" ]; then
+    echo "Error: You don't have PHP installed on your machine." && exit;
+elif [ ! -e "$coveralls_location" ]; then
+    echo "Error: You don't have PHP Coveralls installed - check Composer's dependencies." && exit;
+fi
+
+php "$coveralls_location" "$@";
