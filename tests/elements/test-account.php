d82f6f3440200b2c38a25a58785c3a2fbf39c0ea
Đorđe Jocić <office@djordjejocic.com>
2018-12-25 03:17:36 +0100

    Added unit test for account class.

---
+++ tests/elements/test-account.php
@@ -0,0 +1,142 @@
+<?php
+    
+    /*******************************************************************\
+    |* Author: Djordje Jocic                                           *|
+    |* Year: 2018                                                      *|
+    |* License: MIT License (MIT)                                      *|
+    |* =============================================================== *|
+    |* Personal Website: http://www.djordjejocic.com/                  *|
+    |* =============================================================== *|
+    |* Permission is hereby granted, free of charge, to any person     *|
+    |* obtaining a copy of this software and associated documentation  *|
+    |* files (the "Software"), to deal in the Software without         *|
+    |* restriction, including without limitation the rights to use,    *|
+    |* copy, modify, merge, publish, distribute, sublicense, and/or    *|
+    |* sell copies of the Software, and to permit persons to whom the  *|
+    |* Software is furnished to do so, subject to the following        *|
+    |* conditions.                                                     *|
+    |* --------------------------------------------------------------- *|
+    |* The above copyright notice and this permission notice shall be  *|
+    |* included in all copies or substantial portions of the Software. *|
+    |* --------------------------------------------------------------- *|
+    |* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, *|
+    |* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES *|
+    |* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        *|
+    |* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     *|
+    |* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    *|
+    |* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     *|
+    |* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   *|
+    |* OTHER DEALINGS IN THE SOFTWARE.                                 *|
+    \*******************************************************************/
+    
+    use PHPUnit\Framework\TestCase;
+    use Jocic\GoogleAuthenticator\Account;
+    
+    /**
+     * <i>TestAccount</i> class is used for testing method implementation of the
+     * class <i>Account</i>.
+     * 
+     * @author    Djordje Jocic <office@djordjejocic.com>
+     * @copyright 2018 All Rights Reserved
+     * @version   1.0.0
+     */
+    
+    class TestAccount extends TestCase
+    {
+        /******************\
+        |* CORE CONSTANTS *|
+        \******************/
+        
+        // CORE CONSTANTS GO HERE
+        
+        /******************\
+        |* CORE VARIABLES *|
+        \******************/
+        
+        // CORE VARIABLES GO HERE
+        
+        /*******************\
+        |* MAGIC FUNCTIONS *|
+        \*******************/
+        
+        // MAGIC FUNCTIONS GO HERE
+        
+        /***************\
+        |* GET METHODS *|
+        \***************/
+        
+        // GET METHODS GO HERE
+        
+        /***************\
+        |* SET METHODS *|
+        \***************/
+        
+        // SET METHODS GO HERE
+        
+        /****************\
+        |* CORE METHODS *|
+        \****************/
+        
+        /**
+         * Tests <i>set</i> & <i>get</i> methods of the <i>Secret</i> class.
+         * 
+         * @author    Djordje Jocic <office@djordjejocic.com>
+         * @copyright 2018 All Rights Reserved
+         * @version   1.0.0
+         * 
+         * @return void
+         */
+        
+        public function testGetSetMethod()
+        {
+            // Core Variables
+            
+            $account = new Account();
+            
+            // Other Variables
+            
+            $testServiceNames = [
+                "Hosting ABC",
+                "FastFood Joint",
+                "That Shop"
+            ];
+            
+            $testAccountNames = [
+                "John Doe",
+                "john@doe.com",
+                "Buyer McBuyerson"
+            ];
+            
+            // Step 1 - Test "Service Name" Methods
+            
+            foreach ($testServiceNames as $testServiceName)
+            {
+                $account->setServiceName($testServiceName);
+                
+                $this->assertSame($testServiceName, $account->getServiceName());
+            }
+            
+            // Step 2 - Test "Account Name" Methods
+            
+            foreach ($testAccountNames as $testAccountName)
+            {
+                $account->setAccountName($testAccountName);
+                
+                $this->assertSame($testAccountName, $account->getAccountName());
+            }
+        }
+        
+        /*****************\
+        |* CHECK METHODS *|
+        \*****************/
+        
+        // CHECK METHODS GO HERE
+        
+        /*****************\
+        |* OTHER METHODS *|
+        \*****************/
+        
+        // OTHER METHODS GO HERE
+    }
+    
+?>
